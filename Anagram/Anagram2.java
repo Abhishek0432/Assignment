974de1dd34ab24cdf424d66620697989c24176ea
Abhishek0432 <abhishekupari33@gmail.com>
2025-04-07 16:31:00 -0400

    Example for understanding anagram program

---
+++ Anagram/Anagram2.java
@@ -0,0 +1,31 @@
+package com.cloudvandhana;
+
+import java.util.Arrays;
+
+public class Anagram2 {
+
+	public static void main(String[] args) {
+		String s1 = "hello";
+		String s2 = "world";
+		if(s1.length()==s2.length()) {
+			char[]c1 = s1.toCharArray();
+			char[]c2 = s2.toCharArray();
+			Arrays.sort(c1);
+			Arrays.sort(c2);
+			if(Arrays.equals(c1, c2))
+			{
+				System.out.println("true");
+			}
+			else {
+				System.out.println("false");
+			}
+			
+		}
+		else
+		{
+			System.out.println("false");
+		}
+
+	}
+
+}
